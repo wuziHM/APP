4228643ad3fae4b9d9df8f4e0a26baf3d12a74b1
AllenHu <359222347@qq.com>
2016-03-29 18:20:27 +0800

    修复了横向统计条的bug

--- app/src/main/java/allenhu/app/view/HorizontalBar.java
+++ app/src/main/java/allenhu/app/view/HorizontalBar.java
@@ -3,6 +3,7 @@ package allenhu.app.view;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
@@ -23,19 +24,18 @@ import allenhu.app.util.LogUtil;
 public class HorizontalBar extends View {
 
 
-    private int width;
-    private int height;
     private int oneCount;
     private int twoCount;
     private int threeCount;
     private int fourCount;
     private int fiveCount;
-    private boolean isFirst = true;
     private Rect rect;
     private int bottom = 10;
-    private int mWidth;
-    private int mHeight;
     private int left;
+    private int mWidth;
+    private int mWidth1;
+    private int mHeight;
+    private Rect mBound;
 
     public HorizontalBar(Context context) {
         this(context, null);
@@ -48,8 +48,8 @@ public class HorizontalBar extends View {
     public HorizontalBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-
-
+        left = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics());
+        bottom = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics());
 
         TypedArray array = getContext().getTheme().obtainStyledAttributes(attrs,
                 R.styleable.HorizontalBar, defStyleAttr, 0);
@@ -74,8 +74,8 @@ public class HorizontalBar extends View {
                     fiveCount = array.getInt(attr, 0);
                     break;
                 case R.styleable.HorizontalBar_horHeight:
-//                    bottom = array.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(
-//                            TypedValue.COMPLEX_UNIT_PX, 20, getResources().getDisplayMetrics()));
+                    bottom = array.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(
+                            TypedValue.COMPLEX_UNIT_PX, 20, getResources().getDisplayMetrics()));
                     break;
             }
         }
@@ -131,67 +131,26 @@ public class HorizontalBar extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        if (isFirst) {
+        int specMode = MeasureSpec.getMode(widthMeasureSpec);
+        int specSize = MeasureSpec.getSize(widthMeasureSpec);
 
-            WindowManager wm = (WindowManager) getContext()
-                    .getSystemService(Context.WINDOW_SERVICE);
-            int measureWidth = wm.getDefaultDisplay().getWidth();
-            int measureHeight = wm.getDefaultDisplay().getHeight();
-            /**
-             * 在view里获取有效的屏幕的宽高
-             */
-//            int measureWidth = MeasureSpec.getSize(widthMeasureSpec);
-//            int measureHeight = MeasureSpec.getSize(heightMeasureSpec);
-
-            LogUtil.e("measureWidth：" + measureWidth + "");
-            LogUtil.e("measureHeight：" + measureHeight + "");
-
-            width = measureWidth / 3;
-
-//            setMeasuredDimension(mWidth, mHeight);
-            isFirst = false;
-            setMeasuredDimension(width, 500);
+        LogUtil.e("measureWidth：" + specSize + "");
+        if (specMode == MeasureSpec.EXACTLY) {
+            mWidth = specSize;
+        } else {
+            mWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics());
+            mWidth1 = mWidth + 3 * bottom;
         }
 
+        specMode = MeasureSpec.getMode(heightMeasureSpec);
+        specSize = MeasureSpec.getSize(heightMeasureSpec);
 
-//        int specMode = MeasureSpec.getMode(widthMeasureSpec);
-//        int specSize = MeasureSpec.getSize(widthMeasureSpec);
-//
-//        LogUtil.e("measureWidth：" + specSize + "");
-//        if (specMode == MeasureSpec.EXACTLY) {
-//            mWidth = specSize;
-//        } else {
-//            if (specMode == MeasureSpec.AT_MOST) {
-//                mWidth = specSize;
-//            }
-//            if (specMode == MeasureSpec.UNSPECIFIED) {
-//                mWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 80, getResources().getDisplayMetrics());
-////                mWidth = Math.min(desire, specSize);
-//            }
-//        }
-//
-//        specMode = MeasureSpec.getMode(heightMeasureSpec);
-//        specSize = MeasureSpec.getSize(heightMeasureSpec);
-//
-//        LogUtil.e("measureHeight：" + specSize + "");
-//        if (specMode == MeasureSpec.EXACTLY) {
-//            mHeight = heightMeasureSpec;
-//        } else {
-////            int desireByImg = getPaddingTop() + getPaddingBottom() + mImage.getHeight();
-////            int desireByText = getPaddingTop() + getPaddingBottom() + mTextBound.height();
-//            if (specMode == MeasureSpec.AT_MOST) {
-////                int desire = Math.max(desireByImg, desireByText);
-////                mHeight = Math.min(desire, specSize);
-//                mHeight = specSize;
-//            } else if (specMode == MeasureSpec.UNSPECIFIED) {
-////                int desire = Math.max(desireByImg, desireByText);
-////                mHeight = desire;
-//                mHeight = bottom * 5 + left * 4;
-//            }
-//        }
-//        LogUtil.e("mWidth:"+mWidth+"    mHeight:"+mHeight);
-//        setMeasuredDimension(mWidth, mHeight);
-
+        if (specMode == MeasureSpec.EXACTLY) {
+            mHeight = specSize;
+        } else {
+            mHeight = bottom * 5 + left * 4;
+        }
+        setMeasuredDimension(mWidth1, mHeight);
 
     }
 
@@ -207,62 +166,59 @@ public class HorizontalBar extends View {
         }
 
 
-        left = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getResources().getDisplayMetrics());
-        bottom = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 7, getResources().getDisplayMetrics());
-//        circleWidth = a.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(
-//                TypedValue.COMPLEX_UNIT_PX, 20, getResources().getDisplayMetrics()));
-
-
         rect = new Rect();
-        LogUtil.e("left:" + left);
         rect.left = left;
-
-        LogUtil.e("bottom:" + bottom);
         rect.bottom = bottom;
         rect.top = 0;
-        rect.right = (int) (fiveCount * 1.0f / max * width);
+        rect.right = (int) (fiveCount * 1.0f / max * mWidth);
+
+        Paint textPaint = new Paint();
+        textPaint.setColor(Color.GRAY);
+        textPaint.setAntiAlias(true);
+        textPaint.setStyle(Paint.Style.FILL);
+        mBound = new Rect();
+        textPaint.setTextSize(15);
+        textPaint.getTextBounds("" + max, 0, ("" + max).length(), mBound);
 
         Paint paint1 = new Paint();
         int myColor = getResources().getColor(R.color.fiveStart);
         paint1.setColor(myColor);
         canvas.drawRect(rect, paint1);
-        paint1.setColor(getResources().getColor(R.color.gray));
+        canvas.drawText("" + fiveCount, rect.right + left, (rect.top + rect.bottom) / 2 + mBound.width() / 2, textPaint);
+//        paint1.setColor(getResources().getColor(R.color.gray));
 //        canvas.drawText(fiveCount + "", rect.right + 10, 15, paint1);
 
 
-        Paint paint2 = new Paint();
-        paint2.setColor(getResources().getColor(R.color.fourStart));
+        paint1.setColor(getResources().getColor(R.color.fourStart));
         rect.top = rect.bottom + left;
-        rect.right = (int) (fourCount * 1.0f / max * width);
+        rect.right = (int) (fourCount * 1.0f / max * mWidth);
         rect.bottom = rect.top + bottom;
-        canvas.drawRect(rect, paint2);
-//        canvas.drawText(fourCount + "", rect.right + 10, 37, paint1);
+        canvas.drawRect(rect, paint1);
+        canvas.drawText("" + fourCount, rect.right + left, (rect.top + rect.bottom) / 2 + mBound.width() / 2, textPaint);
 
 
-        Paint paint3 = new Paint();
-        paint3.setColor(getResources().getColor(R.color.threeStart));
+        paint1.setColor(getResources().getColor(R.color.threeStart));
         rect.top = rect.bottom + left;
-        rect.right = (int) (threeCount * 1.0f / max * width);
+        rect.right = (int) (threeCount * 1.0f / max * mWidth);
         rect.bottom = rect.top + bottom;
-        canvas.drawRect(rect, paint3);
-//        canvas.drawText(threeCount + "", right + 10, 59, paint1);
+        canvas.drawRect(rect, paint1);
+        canvas.drawText("" + threeCount, rect.right + left, (rect.top + rect.bottom) / 2 + mBound.width() / 2, textPaint);
 
 
-        Paint paint4 = new Paint();
-        paint4.setColor(getResources().getColor(R.color.twoStart));
+        paint1.setColor(getResources().getColor(R.color.twoStart));
         rect.top = rect.bottom + left;
-        rect.right = (int) (twoCount * 1.0f / max * width);
+        rect.right = (int) (twoCount * 1.0f / max * mWidth);
         rect.bottom = rect.top + bottom;
-        canvas.drawRect(rect, paint4);
+        canvas.drawRect(rect, paint1);
+        canvas.drawText("" + twoCount, rect.right + left, (rect.top + rect.bottom) / 2 + mBound.width() / 2, textPaint);
 
 
-        Paint paint5 = new Paint();
-        paint5.setColor(getResources().getColor(R.color.oneStart));
+        paint1.setColor(getResources().getColor(R.color.oneStart));
         rect.top = rect.bottom + left;
-        rect.right = (int) (oneCount * 1.0f / max * width);
+        rect.right = (int) (oneCount * 1.0f / max * mWidth);
         rect.bottom = rect.top + bottom;
-        canvas.drawRect(rect, paint5);
-//        canvas.drawText(oneCount + "", right + 10, 103, paint1);
+        canvas.drawRect(rect, paint1);
+        canvas.drawText("" + oneCount, rect.right + left, (rect.top + rect.bottom) / 2 + mBound.width() / 2, textPaint);
 
 
     }
