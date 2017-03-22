635c241ac33a699c7a0a66fd415aad7dfc5477f0
MinHu <359222347@qq.com>
2017-03-22 17:24:13 +0800

    统一配置打包信息

--- java_lib/src/main/java/com/example/MyClass.java
+++ java_lib/src/main/java/com/example/MyClass.java
@@ -3,23 +3,7 @@ package com.example;
 public class MyClass {
 
     public static void main(String[] args) {
-//        List list = new ArrayList();
-//        if(list.isEmpty()){
-//            System.out.println("list是空的时候isEmtpty判断是正确的");
-//        }
-
-//        List a = new ArrayList<>();
-//        for (int i = 0; i < 100; i++) {
-//            if (i == 50) {
-//                break;
-//            }
-//            System.out.println(i);
-//        }
-
-        String value = "123";
-        String[] values = value.split(",");
-        System.out.println("size:" + values.length);
-//        int t = AryDemo.calculate(20, 8);
-//        System.out.println("计算结果：" + t);
+        String a = "18874442357";
+        System.out.println(a.replaceAll("(\\d{3})\\d{4}(\\d{4})", "$1****$2"));
     }
 }
